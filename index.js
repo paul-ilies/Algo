1a8186432eb2f33ad9191525ab79fe1d2b24f678
paul-ilies <pauldaniel.ilies@gmail.com>
2022-02-03 22:18:14 +0200

    new algo

--- index.js
+++ index.js
@@ -9,26 +9,26 @@ Return k after placing the final result in the first k slots of nums.
 Do not allocate extra space for another array. You must do this by modifying the input array in-place with O(1) extra memory.
 
 */
-const removeDuplicates = (nums) => {
-    // define indexes of array
-    let i = 0;
-    let j = 1;
-    while (i < nums.length) {
-        if (nums[i] === nums[j]) {
-            // if we find a duplicate, it will be removed and indexes will go back by 1
-            nums.splice(i, 1)
-            i--;
-            j--;
-        }
-        // increment of indexes
-        i++
-        j++
+// const removeDuplicates = (nums) => {
+//     // define indexes of array
+//     let i = 0;
+//     let j = 1;
+//     while (i < nums.length) {
+//         if (nums[i] === nums[j]) {
+//             // if we find a duplicate, it will be removed and indexes will go back by 1
+//             nums.splice(i, 1)
+//             i--;
+//             j--;
+//         }
+//         // increment of indexes
+//         i++
+//         j++
 
-    }
+//     }
 
-}
+// }
 
-console.log(removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]))
+// console.log(removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]))
 
 
 // 27. Remove Element
@@ -42,16 +42,34 @@ Return k after placing the final result in the first k slots of nums.
 Do not allocate extra space for another array. You must do this by modifying the input array in-place with O(1) extra memory.
 */
 
-const removeElement = (nums, val) => {
-    let i = 0;
-    while (i < nums.length) {
-        if (nums[i] === val) {
-            nums.splice(i, 1)
-            i--
-        }
-        i++
-    }
+// const removeElement = (nums, val) => {
+//     let i = 0;
+//     while (i < nums.length) {
+//         if (nums[i] === val) {
+//             nums.splice(i, 1)
+//             i--
+//         }
+//         i++
+//     }
 
-}
+// }
 
-console.log(removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2))
+// console.log(removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2))
+
+// 28. Implement strStr()
+/*
+ Implement strStr().
+
+Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+Clarification:
+
+What should we return when needle is an empty string? This is a great question to ask during an interview.
+ */
+
+// const strStr = (haystack, needle) => {
+//     return needle === "" ? 0 : haystack.indexOf(needle)
+// }
+// console.log(strStr("hello", "ll"))
+
+
