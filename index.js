b7858462de1453a7fe2d4553dca831658bd2958b
paul-ilies <pauldaniel.ilies@gmail.com>
2022-02-04 16:42:11 +0200

    plus one

--- index.js
+++ index.js
@@ -72,4 +72,18 @@ What should we return when needle is an empty string? This is a great question t
 // }
 // console.log(strStr("hello", "ll"))
 
+// 66. Plus One
+/*
+You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer. The digits are ordered from most significant to least significant in left-to-right order. The large integer does not contain any leading 0's.
 
+Increment the large integer by one and return the resulting array of digits.
+
+*/
+// const plusOne = (digits) => {
+//     let convertString = digits.map(el => el.toString())
+//     let partialNum = BigInt(convertString.join("")) + 1n
+//     return partialNum.toString().split("").map(el => parseInt(el))
+
+// }
+
+// console.log(plusOne([6, 1, 4, 5, 3, 9, 0, 1, 9, 5, 1, 8, 6, 7, 0, 5, 5, 4, 3]))
