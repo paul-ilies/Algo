cdfc2aebb8cead295ff6bc00c2743015f01ad155
paul-ilies <pauldaniel.ilies@gmail.com>
2022-02-05 13:17:21 +0200

    sqrt

--- index.js
+++ index.js
@@ -101,3 +101,21 @@ Increment the large integer by one and return the resulting array of digits.
 // }
 // console.log(addBinary("10100000100100110110010000010101111011011001101110111111111101000000101111001110001111100001101",
 //     "110101001011101110001111100110001010100001101011101010000011011011001011101111001100000011011110011"))
+
+// 69. Sqrt(x)
+/*
+Given a non-negative integer x, compute and return the square root of x.
+
+Since the return type is an integer, the decimal digits are truncated, and only the integer part of the result is returned.
+
+Note: You are not allowed to use any built-in exponent function or operator, such as pow(x, 0.5) or x ** 0.5.
+*/
+// const mysqrt = (x) => {
+//     let i = 0;
+
+//     while (i * i <= x) {
+//         i++
+//     }
+//     return i - 1
+// }
+// console.log(mysqrt(4))
