bb51620c429a2260bd78cea0028448606092aea1
Ilies Paul Daniel <pauldaniel.ilies@gmail.com>
2022-02-16 21:26:14 +0200

    fibo1

--- index.js
+++ index.js
@@ -391,3 +391,28 @@ If a string contains all repeating characters, it should return an empty string 
 // }
 
 // console.log(maxChar("abcccccccd"))
+
+
+// --- Directions
+// Print out the n-th entry in the fibonacci series.
+// The fibonacci series is an ordering of numbers where
+// each number is the sum of the preceeding two.
+// For example, the sequence
+//  [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+// forms the first ten entries of the fibonacci series.
+// Example:
+//   fib(4) === 3
+
+// function fib(n) {
+
+//     if (n < 2) {
+//         return n;
+//     }
+//     return fib(n - 1) + fib(n - 2)
+
+// }
+
+// console.log(fib(4))
+
+
+
