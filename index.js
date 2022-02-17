7d7816737a3384ecc7333614efbd1d9663dda3ac
Ilies Paul Daniel <pauldaniel.ilies@gmail.com>
2022-02-17 21:29:45 +0200

    queue and stack algo

--- index.js
+++ index.js
@@ -429,5 +429,115 @@ If a string contains all repeating characters, it should return an empty string 
 
 // console.log(fib2(4))
 
+/*
+225. Implement Stack using Queues
+Implement a last-in-first-out (LIFO) stack using only two queues. The implemented stack should support all the functions of a normal stack (push, top, pop, and empty).
+
+Implement the MyStack class:
+
+void push(int x) Pushes element x to the top of the stack.
+int pop() Removes the element on the top of the stack and returns it.
+int top() Returns the element on the top of the stack.
+boolean empty() Returns true if the stack is empty, false otherwise.
+Notes:
+
+You must use only standard operations of a queue, which means that only push to back, peek/pop from front, size and is empty operations are valid.
+Depending on your language, the queue may not be supported natively. You may simulate a queue using a list or deque (double-ended queue) as long as you use only a queue's standard operations.
+*/
+
+
+// var MyStack = function () {
+//     this.data = [];
+// };
+
+// /**
+//  * @param {number} x
+//  * @return {void}
+//  */
+// MyStack.prototype.push = function (x) {
+//     this.data.push(x)
+// };
+
+// /**
+//  * @return {number}
+//  */
+// MyStack.prototype.pop = function () {
+//     return this.data.pop()
+// };
+
+// /**
+//  * @return {number}
+//  */
+// MyStack.prototype.top = function () {
+//     return this.data[this.data.length - 1]
+// };
+
+// /**
+//  * @return {boolean}
+//  */
+// MyStack.prototype.empty = function () {
+//     if (!this.data.length) {
+//         return true;
+//     } else {
+//         return false
+//     }
+// };
+
+
+/*
+232. Implement Queue using Stacks
+Implement a first in first out (FIFO) queue using only two stacks. The implemented queue should support all the functions of a normal queue (push, peek, pop, and empty).
+
+Implement the MyQueue class:
+
+void push(int x) Pushes element x to the back of the queue.
+int pop() Removes the element from the front of the queue and returns it.
+int peek() Returns the element at the front of the queue.
+boolean empty() Returns true if the queue is empty, false otherwise.
+Notes:
+
+You must use only standard operations of a stack, which means only push to top, peek/pop from top, size, and is empty operations are valid.
+Depending on your language, the stack may not be supported natively. You may simulate a stack using a list or deque (double-ended queue) as long as you use only a stack's standard operations.
+*/
+
+
+// var MyQueue = function () {
+//     this.data = [];
+// };
+
+// /**
+//  * @param {number} x
+//  * @return {void}
+//  */
+// MyQueue.prototype.push = function (x) {
+//     this.data.unshift(x)
+// };
+
+// /**
+//  * @return {number}
+//  */
+// MyQueue.prototype.pop = function () {
+//     return this.data.pop()
+// };
+
+// /**
+//  * @return {number}
+//  */
+// MyQueue.prototype.peek = function () {
+//     return this.data[this.data.length - 1]
+// };
+
+// /**
+//  * @return {boolean}
+//  */
+// MyQueue.prototype.empty = function () {
+//     if (!this.data.length) {
+//         return true
+//     } else {
+//         return false
+//     }
+// };
+
+
 
 
